eb4db4343970947de7ca34072f9750f9c87ee6e9
ChuDuong <chuquangduong191@gmail.com>
2026-09-14 22:36:37 +0700

    Update api_router.py

--- backend/api/api_router.py
+++ backend/api/api_router.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
 
+from backend.api.endpoints import attendance, students, webcams
+
+api_router = APIRouter()
+api_router.include_router(attendance.router)
+api_router.include_router(students.router)
+api_router.include_router(webcams.router)
